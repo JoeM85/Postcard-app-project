b42bc2ad4c47b6a29f1eef1c70a06f3564380e1d
Joe Moran <joe.moran2@aol.com>
2018-09-30 17:33:00 -0400

    update

--- server/app.js
+++ server/app.js
@@ -15,6 +15,17 @@ app.use((req, res, next) => {
   next();
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.post('/send', (req, res) => {
   const { email, image } = req.body;
   const transporter = nodemailer.createTransport({
