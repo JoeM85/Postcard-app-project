deb398cec441378b8f9bf6226bd32953995512c6
Joe Moran <joe.moran2@aol.com>
2018-09-30 17:43:40 -0400

    update

--- server/app.js
+++ server/app.js
@@ -16,13 +16,13 @@ app.use((req, res, next) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
+  // Express will serve up production assets
+  app.use(express.static('build'));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
 }
 
